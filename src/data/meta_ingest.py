61fade1421673a3fa44c3f2611d1823093de9e3c
garthur <graham.d.arthur18@gmail.com>
2019-05-08 14:24:58 -0400

    initial changes

--- src/data/meta_ingest.py
+++ src/data/meta_ingest.py
@@ -1,10 +1,19 @@
 # meta_ingest.py
 
+import pandas as pd
+
 def gen_meta_nodelist(filepath):
+    nl = pd.read_excel(filepath, sheetname=0, 
+                        usecols=["gameid", "side", "position", "champion", 
+                                 "result", "k", "d", "a"])
     pass
 
 def gen_meta_edgelist_ud(filepath):
     pass
 
 def gen_meta_info(filepath):
-    pass
+    meta_info = pd.read_excel(filepath, sheetname=0,
+                              usecols=["gameid", "league", "split", "date", 
+                                       "week", "patchno"])
+
+    return meta_info.drop_duplicates()
