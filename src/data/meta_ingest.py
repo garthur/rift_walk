a914909648c821a8bdc40f26ba27d944406ecebe
garthur <graham.d.arthur18@gmail.com>
2019-05-22 18:36:37 -0400

    only unique gameids

--- src/data/meta_ingest.py
+++ src/data/meta_ingest.py
@@ -1,7 +1,9 @@
 # meta_ingest.py
 
 import os
+import re
 import pathlib
+import urllib.parse
 import dotenv
 import datetime
 import luigi
@@ -11,21 +13,36 @@ import pandas as pd
 dotenv.load_dotenv(dotenv.find_dotenv())
 
 def gen_meta_nodelist(filepath):
-    nl = pd.read_excel(filepath, sheetname=0, 
-                        usecols=["gameid", "side", "position", "champion", 
+    nl = pd.read_excel(filepath, sheet_name=0, 
+                        usecols=["url", "side", "position", "champion", 
                                  "result", "k", "d", "a"])
     # get rid of team observations
     nl = nl[nl["position"] != "Team"]
+    # no valid gameid
+    nl = nl[nl.url.notnull()]
+    nl["gameid"] = nl.url.apply(
+        lambda x: str(urllib.parse.parse_qs(
+            urllib.parse.urlparse(x, allow_fragments=False).query
+        )["gameHash"][0])
+    )
+    nl = nl.drop(["url"], axis=1)
     return nl
 
 def gen_meta_edgelist(filepath):
     import itertools as itr
     # read in the data
-    df = pd.read_excel(filepath, sheetname=0,
-                       usecols=["gameid", "side", "position", "champion", 
+    df = pd.read_excel(filepath, sheet_name=0,
+                       usecols=["url", "side", "position", "champion", 
                                 "ban1", "ban2", "ban3", "ban4", "ban5"])
     # get rid of team observations
     df = df[df["position"] != "Team"]
+    # no valid gameid
+    df = df[df.url.notnull()]
+    df["gameid"] = df.url.apply(
+        lambda x: str(urllib.parse.parse_qs(
+            urllib.parse.urlparse(x, allow_fragments=False).query
+        )["gameHash"][0])
+    )
     
     # initialize empty data frames
     pw = pd.DataFrame()
@@ -65,10 +82,18 @@ def gen_meta_edgelist(filepath):
     return pw.append(ba)
 
 def gen_meta_info(filepath):
-    meta_info = pd.read_excel(filepath, sheetname=0,
-                              usecols=["gameid", "league", "split", "date", 
+    meta_info = pd.read_excel(filepath, sheet_name=0,
+                              usecols=["url", "league", "split", "date", 
                                        "week", "patchno"])
 
+    # no valid gameid
+    meta_info = meta_info[meta_info.url.notnull()]
+    meta_info["gameid"] = meta_info.url.apply(
+        lambda x: str(urllib.parse.parse_qs(
+            urllib.parse.urlparse(x, allow_fragments=False).query
+        )["gameHash"][0])
+    )
+    meta_info = meta_info.drop(["url"], axis=1)
     return meta_info.drop_duplicates()
 
 class MetaDBClean(luigi.Task):
@@ -156,7 +181,6 @@ class MetaDBSetup(luigi.Task):
                 gameid TEXT REFERENCES meta_info(gameid) ON DELETE CASCADE,
                 champ_a TEXT,
                 champ_b TEXT,
-                side TEXT,
                 link_type TEXT
             );
             """
@@ -169,10 +193,10 @@ class MetaDBSetup(luigi.Task):
                 side TEXT,
                 position TEXT,
                 champ TEXT,
-                result integer,
-                k integer CHECK (k >= 0),
-                d integer CHECK (d >= 0),
-                a integer CHECK (a >= 0)
+                result INTEGER,
+                k INTEGER CHECK (k >= 0),
+                d INTEGER CHECK (d >= 0),
+                a INTEGER CHECK (a >= 0)
             );
             """
         )
@@ -189,6 +213,7 @@ class MetaDBSetup(luigi.Task):
 class MetaUpload(luigi.Task):
     overwrite = luigi.BoolParameter(default=False)
     ingest_dir = luigi.Parameter(default="/data/raw")
+    __complete = False
 
     def requires(self):
         return MetaDBSetup(self.overwrite)
@@ -199,13 +224,12 @@ class MetaUpload(luigi.Task):
             host = os.environ.get("AWS_DATABASE_URL"),
             dbname = os.environ.get("AWS_DATABASE_NAME"),
             user = os.environ.get("AWS_DATABASE_USER"),
-            password = os.environ.get("AWS_DATABASE_PW")
+            password = os.environ.get("AWS_DATABASE_PW"),
+            connect_timeout = 18000
         )
 
         cur = conn.cursor()
 
-        print(self.ingest_dir)
-        print(os.path.exists(self.ingest_dir))
         # read file information
         files = []
         for r, d, f in os.walk(self.ingest_dir):
@@ -214,42 +238,51 @@ class MetaUpload(luigi.Task):
                     files.append(os.path.join(r, f_))
 
         # insert sql
-        insert_el = """
-                    INSERT INTO meta_edgelist (gameid, champ_a, champ_b, side, link_type) 
-                    VALUES (%s, %s, %s, %s, %s);
-                    """
-        insert_nl = """
-                    INSERT INTO meta_nodelist (gameid, side, position, champ, result, k, d, a)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
-                    """
+        
+        
         insert_info = """
-                      INSERT INTO meta_info (gameid, league, split, game_date, week, patchno)
+                      INSERT INTO meta_info (league, split, game_date, week, patchno, gameid)
                       VALUES (%s, %s, %s, %s, %s, %s);
                       """
+        insert_el = """
+                    INSERT INTO meta_edgelist (champ_a, champ_b, link_type, gameid) 
+                    VALUES (%s, %s, %s, %s);
+                    """
+        insert_nl = """
+                    INSERT INTO meta_nodelist (side, position, champ, result, k, d, a, gameid)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+                    """
 
         # upload to db
         for f in files:
-            el = gen_meta_edgelist(f)
-            nl = gen_meta_nodelist(f)
-            info = gen_meta_info(f)
-            # edgelist
-            #FIXME: (tuple index out of range)
-            cur.executemany(
-                query = insert_el,
-                vars_list = [tuple(row)[1:] for row in el.itertuples()]
-            )
-            # nodelist
-            cur.executemany(
-                query = insert_nl,
-                vars_list = [tuple(row)[1:] for row in nl.itertuples()]
-            )
+
             # metadata
+            info = gen_meta_info(f)
             cur.executemany(
                 query = insert_info,
                 vars_list = [tuple(row)[1:] for row in info.itertuples()]
             )
-            print(f, "done!")
+            del info
+            # edgelist
+            el = gen_meta_edgelist(f)
+            cur.executemany(
+                query = insert_el,
+                vars_list = [tuple(row)[1:] for row in el.itertuples()]
+            )
+            del el
+            # nodelist
+            nl = gen_meta_nodelist(f)
+            cur.executemany(
+                query = insert_nl,
+                vars_list = [tuple(row)[1:] for row in nl.itertuples()]
+            )
+            del nl
 
         # close connection
         conn.commit()
         conn.close()
+
+        self.__complete = True
+
+    def complete(self):
+        return self.__complete
