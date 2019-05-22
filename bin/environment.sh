cdb78cbdb3a69d39e255e5b8632060c9e77684c0
Graham Arthur <graham.d.arthur18@gmail.com>
2019-05-22 19:03:23 -0400

    fixing issues with env creation

--- bin/environment.sh
+++ bin/environment.sh
@@ -3,4 +3,4 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-conda env export | grep -v "^prefix: " > ../environment.yml
+conda env export --no-builds | grep -v "^prefix: " > ../environment.yml
