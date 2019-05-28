fb7e8cc74cb54f9a5c6d918ef720ddec6af54dc5
garthur <graham.d.arthur18@gmail.com>
2019-05-28 00:11:23 -0400

    file ingest changed to pyspark

--- src/data/meta_ingest.py
+++ src/data/meta_ingest.py
@@ -3,6 +3,7 @@
 import os
 import pathlib
 import urllib.parse
+import itertools as itr
 import luigi
 import psycopg2
 import pandas as pd
@@ -14,6 +15,9 @@ dotenv.load_dotenv(dotenv.find_dotenv())
 # spark setup
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.types import ArrayType, StringType
+
 CONF = SparkConf().setAppName("meta_ingest").setMaster("local")
 SC = SparkContext(conf=CONF)
 SQL_CONTEXT = SparkSession(SC)
@@ -21,25 +25,7 @@ SQL_CONTEXT = SparkSession(SC)
 # constants
 MAJOR_LEAGUES = ["NALCS", "LCS", "EUCLS", "LEC", "LPL", "LMS", "LCK", "WC", "MSI"]
 
-def __make_gameid(r):
-    if r.isnull()[1]:
-        gameid = r[0]
-    else:
-        parsed = urllib.parse.parse_qs(
-            urllib.parse.urlparse(r[1], allow_fragments=False).query
-        )
-
-        if "gameHash" in parsed:
-            gameid = str(parsed["gameHash"][0])
-        else:
-            gameid = r[0]
-    
-    r_ = [gameid, gameid]
-    r_.extend(r[2:])
-    
-    return r_
-
-def __validate_gameid(gameid, url):
+def __make_gameid(gameid, url):
     parsed = urllib.parse.parse_qs(
                 urllib.parse.urlparse(url, allow_fragments=False).query
              )
@@ -54,106 +40,19 @@ def __update_leagues(l):
     
     return l_dict[l]
 
-def gen_meta_info(f):
-    meta_info = pd.read_excel(f, sheet_name=0,
-                                usecols=["gameid", "url", "league", "split", "date", 
-                                        "week", "patchno"],
-                                dtype={"gameid":str, "url":str, "league":str, "split":str,
-                                        "week":str, "patchno":str})
-
-    meta_info = meta_info.apply(__make_gameid, axis=1, result_type="broadcast")
-    # keep only major league games
-    meta_info = meta_info[meta_info["league"].isin(MAJOR_LEAGUES)]
-    # update league names
-    meta_info.league = meta_info.league.apply(__update_leagues)
-    # drop unecessary columns
-    meta_info = meta_info.drop(["url"], axis=1)
-    
-    # drop duplicates
-    return meta_info.drop_duplicates()
-
-def gen_meta_nodelist(f, meta_info):
-    nl = pd.read_excel(f, sheet_name=0, 
-                        usecols=["gameid", "url", "side", "position", "champion", 
-                                 "result", "k", "d", "a"],
-                        dtype={"gameid":str, "url":str, "side":str, "position":str, 
-                                "champion":str, "result":int, "k":int, "d":int, "a":int})
-    # get rid of team observations
-    nl = nl[nl["position"] != "Team"]
-    # make and select valid gameid
-    nl = nl.apply(__make_gameid, axis=1, result_type="broadcast")
-    nl = nl[nl["gameid"].isin(meta_info.gameid.values)]
-    # drop unecessary columns
-    nl = nl.drop(["url"], axis=1)
-    return nl
-
-def gen_meta_edgelist(f, meta_info):
-    import itertools as itr
-    # read in the data
-    df = pd.read_excel(f, sheet_name=0,
-                       usecols=["gameid", "url", "side", "position", "champion", 
-                                "ban1", "ban2", "ban3", "ban4", "ban5"],
-                       dtype={"gameid":str, "url":str, "side":str, "position":str, "champion":str,
-                              "ban1":str, "ban2":str, "ban3":str, "ban4":str, "ban5":str})
-    # get rid of team observations
-    df = df[df["position"] != "Team"]
-    # make and select valid gameid
-    df = df.apply(__make_gameid, axis=1, result_type="broadcast")
-    df = df[df["gameid"].isin(meta_info.gameid.values)]
-    # drop unecessary columns
-    df = df.drop(["url"], axis=1)
-    
-    # initialize empty data frames
-    pw = pd.DataFrame()
-    ba = pd.DataFrame()
-
-    def gen_pw(group_df):
-        new = pd.DataFrame()
-        new["pick_combos"] = list(itr.combinations(group_df.champion, 2))
-        
-        new[["champ_a", "champ_b"]] = pd.DataFrame(new.pick_combos.values.tolist(), 
-                                                        index=new.index)
-        new = new.drop(["pick_combos"], axis = 1)
-        new["link_type"] = "pick"
-        new["gameid"] = group_df.gameid.values[0]
-        return new
-    
-    def gen_ba(group_df):
-        new = pd.DataFrame()
-        # loop through ban columns
-        ban_combos = []
-        for ban in range(1,6):
-            ban_combos.extend(list(zip(group_df.champion.values, eval("group_df.ban%s.values" % ban))))
-        new["ban_combos"] = ban_combos
-        new[["champ_a", "champ_b"]] = pd.DataFrame(new.ban_combos.values.tolist(), 
-                                                        index=new.index)
-        new = new.drop(["ban_combos"], axis = 1)
-        new["link_type"] = "ban"
-        new["gameid"] = group_df.gameid.values[0]
-        return new
-    
-    # loop through all the groups
-    for name, group in df.groupby(["gameid", "side"], as_index=False):
-        pw = pw.append(gen_pw(group))
-        # grab banned against
-        ba = ba.append(gen_ba(group))
-
-    return pw.append(ba)
-
 def read_oracle_data(f):
-    
     # read in the data
     df = SQL_CONTEXT.read.csv(f, header=True)
-    
+
     # drop unnecessary columns
     df = df.select("gameid", "url", "league", "split", "date", "week", "patchno",
-                   "side", "position", "champion", "result", "k", "d", "a",
-                   "ban1", "ban2", "ban3", "ban4", "ban5").rdd
+                    "side", "position", "champion", "result", "k", "d", "a",
+                    "ban1", "ban2", "ban3", "ban4", "ban5").rdd
     # filter rows
     df = df.filter(lambda x: (x[2] in MAJOR_LEAGUES) and (x[8] != "Team"))
     # update league and gameid
     df = df.map(lambda x: (*x[0:2], __update_leagues(x[2]), *x[3:]))\
-           .map(lambda x: (__validate_gameid(x[0], x[1]), *x[1:]))
+           .map(lambda x: (__make_gameid(x[0], x[1]), *x[1:]))
 
     # metadata frame
     info = df.map(lambda x: (x[0], *x[2:7]))\
@@ -162,14 +61,46 @@ def read_oracle_data(f):
 
     # nodelist frame
     node = df.map(lambda x: (x[0], *x[7:14]))\
-             .toDF("gameid", "side", "position", "champion", "result", "k", "d", "a")
+             .toDF(["gameid", "side", "position", "champion", "result", "k", "d", "a"])
 
     # edgelist frame
-    ban = df.map(lambda x: (x[0], (x[9], x[14]), (x[9], x[15]), (x[9], x[16]),
-                            (x[9], x[17]), (x[9], x[18]), "ban" ))
+
+    ## get bans
+    bans = df.map(lambda x: (x[0], [[x[9], x[14]], [x[9], x[15]], [x[9], x[16]],
+                            [x[9], x[17]], [x[9], x[18]]], "ban" ))\
+             .toDF(["gameid", "combo", "link_type"])\
+             .select("gameid", F.explode("combo").alias("combo"), "link_type")
     
-    return info, node, None
+    bans = bans.select("gameid",
+                       bans.combo.getItem(0).alias("champ_a"),
+                       bans.combo.getItem(1).alias("champ_b"),
+                       "link_type")
     
+    ## get picks and picked against
+    def __make_champ_combos(champs, sides):
+        cs = list(zip(champs, sides))
+        cs = list(itr.combinations(cs, 2))
+        return [(p[0][0], p[1][0], "pkw" if p[0][1] == p[1][1] else "pka") for p in cs]
+
+    champ_combo = F.udf(__make_champ_combos, ArrayType(ArrayType(StringType())))
+
+    picks = df.map(lambda x: (x[0], x[7], x[9]))\
+              .toDF(["gameid", "side", "champion"])\
+              .groupBy("gameid")\
+              .agg(F.collect_list("champion").alias("champ"), 
+                   F.collect_list("side").alias("side"))\
+              .select("gameid", F.explode(champ_combo(F.col("champ"), F.col("side"))).alias("combo"))
+
+    picks = picks.select("gameid", 
+                         picks.combo.getItem(0).alias("champ_a"),
+                         picks.combo.getItem(1).alias("champ_b"),
+                         picks.combo.getItem(2).alias("link_type"))
+
+    ## append picks and bans
+    edge = picks.union(bans)
+
+    # return all three frames
+    return info, node, edge
 
 class MetaDBClean(luigi.Task):
     __complete = False
