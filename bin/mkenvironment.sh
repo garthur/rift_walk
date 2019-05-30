aa7e49e960fd21f126b441e417049b491dba96c4
garthur <graham.d.arthur18@gmail.com>
2019-05-30 13:13:50 -0400

    utility

--- bin/mkenvironment.sh
+++ bin/mkenvironment.sh
@@ -4,3 +4,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 conda env create -f ../environment.yml
+
+deactivate
+source activate rift_walk_env
+activate rift_walk_env
